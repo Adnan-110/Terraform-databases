599c9ea7899b89a552b68361a6e7a3ee4e0347c5
Adnan <adnanshaikh11501@gmail.com>
2024-01-13 17:26:14 +0530

    Terraform-databases backend,docdb,statefile share

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "docdb"{
+  source                = "./vendor/modules/docdb"
+  ENV                   = var.ENV
+}
+
+# We cannot parameterize anything that's added in the source
+# To limit that, we can use a tool called as Terrafile. All it does is that it clones the remote code from the specified branch and make it locally available.
