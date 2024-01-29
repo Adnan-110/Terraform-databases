671c8dccbac9d4332abe94cbed5f633847792dbd
Adnan <adnanshaikh11501@gmail.com>
2024-01-29 18:36:29 +0530

    DOCDB MYSQL REDIS ENDPOINT Output

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "DOCDB_ENDPOINT" {
+  value = module.docdb.DOCDB_ENDPOINT    
+  }
+
+output "MYSQL_ENDPOINT" {
+  value = module.mysql.MYSQL_ENDPOINT
+}
+
+output "REDIS_ENDPOINT" {
+  value = module.redis.REDIS_ENDPOINT
+}
